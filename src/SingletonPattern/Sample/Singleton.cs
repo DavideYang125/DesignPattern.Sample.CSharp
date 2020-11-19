67a9db9f57fa3ccb11068044990b2c7c3de9bbfc
yangww <dayang_techmail@163.com>
2020-11-19 11:46:10 +0800

    singleton

---
+++ src/SingletonPattern/Sample/Singleton.cs
@@ -0,0 +1,35 @@
+﻿namespace SingletonPattern.Sample
+{
+    public class Singleton
+    {
+        /// <summary>
+        /// 类的实例
+        /// </summary>
+        private static Singleton _singleton;
+        private static readonly object _locker = new object();
+
+        /// <summary>
+        /// 确保构造函数不被外界访问
+        /// </summary>
+        private Singleton()
+        {
+        }
+
+        /// <summary>
+        /// 向外界提供全局的访问点
+        /// </summary>
+        /// <returns></returns>
+        public Singleton GetSingleton()
+        {
+            lock (_locker)
+            {
+                //确保在多线程的情况下，只有一个线程创建实例
+                if (_singleton is null)
+                {
+                    _singleton = new Singleton();
+                }
+            }
+            return _singleton;
+        }
+    }
+}
