2c099422c4ccf23157edaa99eb11c892d5c0464d
yangww <dayang_techmail@163.com>
2020-11-20 15:44:20 +0800

    StrategyPattern

---
+++ src/StrategyPattern/BaseSample/StrategySample.cs
@@ -0,0 +1,70 @@
+﻿/**
+ 策略模式包含如下角色：
+    Context: 环境类
+    Strategy: 抽象策略类
+    ConcreteStrategy: 具体策略类
+ * **/
+using System;
+
+namespace StrategyPattern.BaseSample
+{
+    /// <summary>
+    /// 抽象策略类、接口
+    /// </summary>
+    public interface IStrategy
+    {
+        string Algorithm();
+    }
+
+    /// <summary>
+    /// 具体策略类A
+    /// </summary>
+    public class ConcreteStrategyA : IStrategy
+    {
+        public string Algorithm()
+        {
+            return "ConcreteStrategyA";
+        }
+    }
+
+    /// <summary>
+    /// 具体策略类B
+    /// </summary>
+    public class ConcreteStrategyB : IStrategy
+    {
+        public string Algorithm()
+        {
+            return "ConcreteStrategyB";
+        }
+    }
+
+    /// <summary>
+    /// 环境类
+    /// </summary>
+    public class Context
+    {
+        private IStrategy _strategy;
+
+        public Context(IStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void AlgorithmInvoke()
+        {
+            if (_strategy is null) throw new ArgumentNullException("strategy is null");
+            Console.WriteLine(_strategy.Algorithm());
+        }
+    }
+
+    /// <summary>
+    /// 结合泛型 另一种用法
+    /// </summary>
+    public class NewContext
+    {
+        public void AlgorithmInvoke<T>(T t) where T : IStrategy
+        {
+            Console.WriteLine(t.Algorithm());
+        }
+    }
+}
---
+++ src/StrategyPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using StrategyPattern.BaseSample;
+
+namespace StrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Context context = new Context(new ConcreteStrategyA());
+            context.AlgorithmInvoke();
+
+
+            NewContext newContext = new NewContext();
+            newContext.AlgorithmInvoke(new ConcreteStrategyB());
+
+            Console.ReadKey();
+
+        }
+
+        
+    }
+}
