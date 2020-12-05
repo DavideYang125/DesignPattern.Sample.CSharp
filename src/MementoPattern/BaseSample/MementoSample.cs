7f11fd6e06c83e62ed2d1766edd3bebb856a38e7
yangww <dayang_techmail@163.com>
2020-12-05 14:22:40 +0800

    memento

---
+++ src/MementoPattern/BaseSample/MementoSample.cs
@@ -0,0 +1,44 @@
+﻿/**
+备忘录模式中主要有三类角色：
+发起人角色：记录当前时刻的内部状态，负责创建和恢复备忘录数据。
+备忘录角色：负责存储发起人对象的内部状态，在进行恢复时提供给发起人需要的状态。
+管理者角色：负责保存备忘录对象。
+* **/
+namespace MementoPattern.BaseSample
+{
+    /// <summary>
+    /// 备忘录
+    /// </summary>
+    public class Memento
+    {
+        public string state { get; set; }
+
+        public Memento(string state)
+        {
+            this.state = state;
+        }
+    }
+
+    /// <summary>
+    /// 发起人
+    /// </summary>
+    public class Originator
+    {
+        public string State { get; set; }
+
+        public Memento CreateMemento()
+        {
+            return new Memento(State);
+        }
+
+        public void RestoreMemento(Memento memento)
+        {
+            State = memento.state;
+        }
+    }
+
+    public class Caretaker
+    {
+        public Memento Memento { get; set; }
+    }
+}
---
+++ src/MementoPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MementoPattern.BaseSample;
+
+namespace MementoPattern
+{
+    /// <summary>
+    /// 备忘录模式
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BaseSample();
+        }
+
+        public static void BaseSample()
+        {
+            Originator originator = new Originator();
+            originator.State = "state1";
+            Console.WriteLine($"originator的状态 {originator.State}");
+            Caretaker caretaker = new Caretaker();
+            caretaker.Memento = originator.CreateMemento();
+            originator.State = "state2";
+            Console.WriteLine($"originator的状态 {originator.State}");
+            Console.WriteLine("开始通过备忘录恢复");
+            originator.RestoreMemento(caretaker.Memento);
+            Console.WriteLine($"originator的状态 {originator.State}");
+
+            Console.ReadKey();
+        }
+    }
+}
