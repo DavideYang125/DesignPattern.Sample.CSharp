d2c49fa9ea5ccf7cb17ff48a5d070b60c103d09a
yangww <dayang_techmail@163.com>
2020-12-10 14:54:48 +0800

    command pattern

---
+++ src/CommandPattern/BaseSample/CommandSample.cs
@@ -0,0 +1,71 @@
+﻿/**
+ 模式结构
+    客户角色：发出一个具体的命令并确定其接受者。
+    命令角色：声明了一个给所有具体命令类实现的抽象接口
+    具体命令角色：定义了一个接受者和行为的弱耦合，负责调用接受者的相应方法。
+    请求者角色：负责调用命令对象执行命令。
+    接受者角色：负责具体行为的执行。
+ * **/
+using System;
+
+namespace CommandPattern.BaseSample
+{
+    /// <summary>
+    /// 抽象命令类
+    /// </summary>
+    public abstract class Command
+    {
+        protected Receiver _receiver;
+
+        protected Command(Receiver receiver)
+        {
+            _receiver = receiver;
+        }
+
+        public abstract void Action();
+    }
+
+    /// <summary>
+    /// 具体命令类
+    /// </summary>
+    public class ConcreteCommand : Command
+    {
+        public ConcreteCommand(Receiver receiver) : base(receiver)
+        {
+        }
+
+        public override void Action()
+        {
+            _receiver.Run();
+        }
+    }
+
+    /// <summary>
+    /// 命令接收者
+    /// </summary>
+    public class Receiver
+    {
+        public void Run()
+        {
+            Console.WriteLine($"{this.GetType().Name} run");
+        }
+    }
+
+    /// <summary>
+    /// 请求者
+    /// </summary>
+    public class Invoke
+    {
+        public Command _command;
+
+        public Invoke(Command command)
+        {
+            _command = command;
+        }
+
+        public void ExecuteCommand()
+        {
+            _command.Action();
+        }
+    }
+}
---
+++ src/CommandPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CommandPattern.BaseSample;
+
+namespace CommandPattern
+{
+    /// <summary>
+    /// 命令模式
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BaseSample();
+        }
+
+        public static void BaseSample()
+        {
+            var receiver = new Receiver();
+            var command = new ConcreteCommand(receiver);
+            var invoke = new Invoke(command);
+            invoke.ExecuteCommand();
+            Console.ReadKey();
+        }
+    }
+}
