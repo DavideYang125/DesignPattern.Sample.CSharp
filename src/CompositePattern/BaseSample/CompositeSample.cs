63705393c1f294f9938e978d68d8951778a8087f
yangww <dayang_techmail@163.com>
2021-01-07 14:52:52 +0800

    composite

---
+++ src/CompositePattern/BaseSample/CompositeSample.cs
@@ -0,0 +1,74 @@
+﻿/*
+    模式结构
+    抽象构件（Component）
+    树叶构件（Leaf）
+    树枝构件（Composite）
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace CompositePattern.BaseSample
+{
+    /// <summary>
+    /// 抽象构件
+    /// </summary>
+    public abstract class Component
+    {
+        public virtual string Name { get; set; }
+
+        public abstract void Add(Component child);
+
+        public abstract void Remove(Component child);
+
+        public abstract void Display(int depth = 0);
+    }
+
+    /// <summary>
+    /// 树叶构件
+    /// </summary>
+    public class Leaf : Component
+    {
+        public override void Add(Component child)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Display(int depth = 0)
+        {
+            Console.WriteLine(new String('-', depth * 2) + Name);
+        }
+
+        public override void Remove(Component child)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// 树枝构件
+    /// </summary>
+    public class Composite : Component
+    {
+        private List<Component> chilren = new List<Component>();
+
+        public override void Add(Component child)
+        {
+            if (child != null) chilren.Add(child);
+        }
+
+        public override void Display(int depth = 0)
+        {
+            Console.WriteLine(new String('-', depth * 2) + Name);
+            chilren.ForEach(x =>
+            {
+                x.Display(depth + 1);
+            });
+        }
+
+        public override void Remove(Component child)
+        {
+            if (chilren != null) chilren.Remove(child);
+        }
+    }
+}
---
+++ src/CompositePattern/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CompositePattern.BaseSample;
+
+namespace CompositePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            BaseSample();
+        }
+
+        public static void BaseSample()
+        {
+            Component root = new Composite();
+            root.Add(new Leaf { Name = "Leaf 1" });
+            root.Add(new Leaf { Name = "Leaf 2" });
+
+            var com = new Composite();
+            root.Add(new Leaf { Name = "Leaf 10" });
+            root.Add(new Leaf { Name = "Leaf 11" });
+
+            var newCom = new Composite();
+            newCom.Add(new Leaf { Name = "Leaf101" });
+            newCom.Add(new Leaf { Name = "Leaf102" });
+
+            com.Add(newCom);
+
+            root.Add(com);
+            var leafA = new Leaf { Name = "Leaf A" };
+            root.Add(leafA);
+
+            root.Add(new Leaf { Name = "Leaf D" });
+
+            root.Display();
+
+            Console.WriteLine("再次展示");
+
+            root.Remove(leafA);
+            root.Display();
+            Console.ReadLine();
+        }
+    }
+}
